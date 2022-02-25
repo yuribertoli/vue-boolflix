da5b63ca2e33a9c663132fdf02c7a704a067087c
yuribertoli <yuribertoli@yahoo.it>
2022-02-25 17:11:59 +0100

    milestone 1

--- src/components/partials/MyFilm.vue
+++ src/components/partials/MyFilm.vue
@@ -2,7 +2,11 @@
  
     <ul>
         <li v-for="(film, indice) in mostraRicerca" :key="indice">
-            <img :src="`http://image.tmdb.org/t/p/w${widthMovie}${film.poster_path}`" alt="">
+        <!-- <img :src="`http://image.tmdb.org/t/p/w${widthMovie}${film.poster_path}`" :alt="film.title"> -->
+            <h3 class="titolo">{{film.title}}</h3>
+            <h3 class="titolo-originale">{{film.original_title}}</h3>
+            <span class="lingua">{{film.original_language}}</span>
+            <span class="voto">{{film.vote_average}}</span>
         </li>
     </ul>
 
