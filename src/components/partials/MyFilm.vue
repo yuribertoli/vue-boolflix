50c8e45edf392d6383742c877188ac5539330e58
yuribertoli <yuribertoli@yahoo.it>
2022-02-25 17:56:28 +0100

    flags

--- src/components/partials/MyFilm.vue
+++ src/components/partials/MyFilm.vue
@@ -5,7 +5,9 @@
         <!-- <img :src="`http://image.tmdb.org/t/p/w${widthMovie}${film.poster_path}`" :alt="film.title"> -->
             <h3 class="titolo">{{film.title}}</h3>
             <h3 class="titolo-originale">{{film.original_title}}</h3>
-            <span class="lingua">{{film.original_language}}</span>
+
+            <!-- aggiungo le bandiere in base alla lingua  -->
+            <span :class="film.original_language == 'en'?'fi fi-gb' : `fi fi-${film.original_language}`"></span>
             <span class="voto">{{film.vote_average}}</span>
         </li>
     </ul>
@@ -24,10 +26,36 @@ export default {
         return{
             widthMovie: 200
         }
+    },
+
+    methods: {
+        getClass(){
+            return 
+        }
     }
 }
 </script>
 
 <style scoped lang="scss">
+@import '~flag-icons/css/flag-icons.css';
 
+ul {
+    width: 70%;
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+
+    li {
+        width: calc(100% / 3);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 300px;
+        border: 1px solid black;
+        background-color: wheat;
+
+
+    }
+}
 </style>
