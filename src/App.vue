aec17d306f0d4bedaaa628760e8c58a065f50a06
yuribertoli <yuribertoli@yahoo.it>
2022-02-25 15:38:34 +0100

    methods header

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
 import MyHeader from './components/MyHeader.vue'
 import MyMain from './components/MyMain.vue'
 
+const axios = require('axios').default;
+
 export default {
   name: 'App',
   components: {
@@ -17,11 +19,34 @@ export default {
   },
   data(){
     return{
-
+      endpointBeginning: "https://api.themoviedb.org/3/search/movie?api_key=",
+      endpointKey: "2bec85c057b8f21a5e08ba0390ff42d0",
+      endpointQuery: "&query=",
+      endpointSearch: "",
+      endpointLanguage: "&language=",
+      endpointChooseLanguage: ""
     }
   },
   methods: {
-      
+
+      getAxios(){
+
+        //creo dinamicamente il percorso 
+        axios.get(this.endpointBeginning + 
+                  this.endpointKey + 
+                  this.endpointQuery + 
+                  this.endpointSearch + 
+                  this.endpointLanguage + 
+                  this.endpointChooseLanguage)
+
+        .then((response) => {
+          console.log(response);
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
+      }
   }
 }
 </script>
--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,5 +1,8 @@
 <template>
-    <header></header>
+    <header> <!-- richiamo la funzione ricerca sia al click del bottone, sia alla pressione del tasto Enter(Invio) -->
+        <input v-on:keyup.enter="ricerca" type="text" v-model="cerca">
+        <button @click="ricerca">Cerca</button>
+    </header>
 </template>
 
 <script>
@@ -13,11 +16,15 @@ export default {
     },  
     data(){
         return{
-
+            cerca: ""
         }
     },
     methods: {
-      
+        ricerca(){ //se il valore di cerca è diverso da vuoto invio ad App la stringa ricercata
+            if (this.cerca != "") {
+                this.$emit('invioRicerca', this.cerca);
+            }
+        }
     }
 }
 </script>
